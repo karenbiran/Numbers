71f7daca79f1897fc709ff5dab2860bb4cb4b8e7
karenbiran <karenbiran@yahoo.com>
2019-08-12 22:26:18 +0300

    Create calendar.h

---
+++ C/Exercise 7/calendar.h
@@ -0,0 +1,18 @@
+#ifndef calendar.h
+#define calendar.h
+
+typedef struct 
+{
+	meeting_t **arr;
+	int count;
+	int capacity;
+}
+calendar_t;
+
+calendar_t* createDA(int capacity);
+int insertMeeting(calendar_t* DA,meeting_t* pMeeting);
+void printAD(calendar_t* DA);
+calendar_t* removeMeeting(calendar_t* ad,meeting_t* pMeeting);
+meeting_t* findMeeting(calendar_t* DA,meeting_t* pMeeting);
+
+#endif
