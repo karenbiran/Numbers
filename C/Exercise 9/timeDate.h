0b7adc414469359c8a5c282449157f9c71a4c379
karenbiran <karenbiran@yahoo.com>
2019-08-15 01:18:53 +0300

    Update timeDate.h

--- C/Exercise 9/timeDate.h
+++ C/Exercise 9/timeDate.h
@@ -1,1 +1,30 @@
+#ifndef timeDate
+#define timeDate
+
+typedef struct
+{
+    int day;
+    int month;
+    int year;
+}
+date_t;
+
+typedef struct
+{
+    int sec;
+    int min;
+    int hour;
+}
+time_t;
+
+date_t* Create_Date();
+void addDate(date_t *date1,int addDays);
+int CheckDate(int dd,int mm,int yy);
+date_t* updateDate(date_t *date1)
+int getDay(date_t *d1);
+int getMonth(date_t *d1);
+int getYear(date_t *d1);
+void printdate(date_t *d1);
+
+#endif
 
