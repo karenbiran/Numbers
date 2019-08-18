d50d746b60c8d533d3b50d89736fc92e32ca57d7
karenbiran <karenbiran@yahoo.com>
2019-08-18 22:04:17 +0300

    Update Bitwise.h

--- C/Exercise 11/Bitwise.h
+++ C/Exercise 11/Bitwise.h
@@ -3,7 +3,7 @@
 
 
 int inverts(unsigned char* c);
-int rotates(unsigned char* c,int n);
+int rotates(unsigned char c,int n);
 int setBits(unsigned char* x,int p,int n,unsigned char* y);
 int setBits2(unsigned char* x,int i,int j,int value);
 
